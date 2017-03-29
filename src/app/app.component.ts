a15d539720418224272266a38581381e66293f42
Andrew Devlin and Ash Laidlaw <ash@>
2017-03-29 09:38:52 -0700

    Add basic site structure and Keg class

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+  kegs: Keg[] = [
+  new Keg("Bud Light", "Budweiser", 4, 4, "Lager"),
+  new Keg("pub drought", "Guinness", 5, 4, "Stout"),
+  new Keg("Boch", "Shiner", 6, 4, "Lager"),
+  new Keg("Rpm", "Boneyard", 7, 6, "ipa"),
+  ];
+
+  priceColor(keg) {
+    if (keg.price <= 3) {
+      return "red";
+    } else if (keg.price <= 5) {
+      return "green";
+    } else {
+      return "blue";
+    }
+  }
+
+  strength(keg) {
+    if (keg.strength >= 6) {
+      return "strong";
+    } else {
+      return "weak";
+    }
+  }
+}
+
+export class Keg {
+  volume: number = 124;
+
+  constructor(public name: string, public brand: string, public price: number, public strength: number, public type: string) {}
+}
